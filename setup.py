76e10d43bf95380ac44f83e8160d9542c2156c5d
kanhataak97@gmail.com <kanhataak97@gmial.com>
2021-11-26 17:08:25 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name= "src", version="0.0.1",
+    description="its a wine Q package",
+    author="kanhataak",
+    packages=find_packages(),
+    license="MIT"
+)
---
+++ tests/test_config.py
@@ -0,0 +1,4 @@
+def test_generic():
+    a = 3
+    b = 3
+    assert a==b
